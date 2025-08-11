05f1700eee1d8a9c4b62e9ec41306dca2063ed39
Ammad IIUI <ammadbaryar@gmail.com>
2025-08-11 23:26:33 +0500

    Setup the Project Folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+let name = "KnoteBook";
+  return (
+    <>
+      <h1 style={ { textAlign : 'center'}}>This is my new {name} App </h1>
+    </>
+  )
+}
+
+export default App
