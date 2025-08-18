8fb40ad87768dadcb44b78886be4091217cb3be9
Ammad IIUI <ammadbaryar@gmail.com>
2025-08-18 20:45:51 +0500

    Add responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import knotebookLogo from '../assets/knotebook.png';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="nav-container">
       <h1 className="nav-heading">
@@ -11,14 +12,21 @@ const Navbar = () => {
           <img src={knotebookLogo} alt="Knotebook" style={{ height: '40px' }} />
         </Link>
       </h1>
-      <div className="nav-links">
-        <Link to="/" className="nav-link">
+      <button
+        className="nav-toggle"
+        aria-label="Toggle menu"
+        onClick={() => setMenuOpen((open) => !open)}
+      >
+        <span className="nav-toggle-icon">&#9776;</span>
+      </button>
+      <div className={`nav-links${menuOpen ? ' open' : ''}`}>
+        <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>
           Home
         </Link>
-        <Link to="/editor" className="nav-link">
+        <Link to="/editor" className="nav-link" onClick={() => setMenuOpen(false)}>
           Editor
         </Link>
-        <Link to="/notes" className="nav-link">
+        <Link to="/notes" className="nav-link" onClick={() => setMenuOpen(false)}>
           Notes
         </Link>
       </div>
